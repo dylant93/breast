0a5f371d45cea3785b06245e6f64eeb7aba1061a
dylant93 <39739012+dylant93@users.noreply.github.com>
2019-07-27 22:03:29 +0800

    Update my_model2.py

--- my_model2.py
+++ my_model2.py
@@ -129,7 +129,7 @@ def model(x, keep_prob, img_size, colour_channels, filter_size, neurons, num_cla
         layer_conv1 = new_conv_layer(
             x_image,
             num_input_channels=colour_channels,
-            filter_size=3,
+            filter_size=1,
             num_filters=64,
             stride=1,
             use_pooling=True,
@@ -143,12 +143,12 @@ def model(x, keep_prob, img_size, colour_channels, filter_size, neurons, num_cla
         layer_conv2 = new_conv_layer(
             layer_conv1,
             num_input_channels=64,
-            filter_size=3,
+            filter_size=2,
             num_filters=128,
-            stride=1,
+            stride=2,
             use_pooling=True,
             f=2,
-            s=2,
+            s=1,
             pad=0,
             poolpad=0
         )
@@ -157,9 +157,9 @@ def model(x, keep_prob, img_size, colour_channels, filter_size, neurons, num_cla
         layer_conv3 = new_conv_layer(
             layer_conv2,
             num_input_channels=128,
-            filter_size=3,
-            num_filters=128,
-            stride=1,
+            filter_size=2,
+            num_filters=256,
+            stride=2,
             use_pooling=True,
             f=2,
             s=2,
@@ -254,7 +254,7 @@ def restore_or_initialize(session, saver, checkpoint_dir):
         tf.global_variables_initializer().run()
 
 
-def train(img_dir, model_dir, img_size=64, colour_channels=3, batch_size=128, training_epochs=50):
+def train(img_dir, model_dir, img_size=32, colour_channels=3, batch_size=128, training_epochs=50):
 
     log_dir = os.path.join(os.path.abspath(model_dir), 'tensorflow/cnn/logs/cnn_with_summaries')
     checkpoint_dir = os.path.join(os.path.abspath(model_dir), 'tensorflow/cnn/model')
